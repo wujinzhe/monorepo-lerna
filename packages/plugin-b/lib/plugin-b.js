1abed56780cbd8d26d9c15969162f3305c62b91f
吴晋哲 <jinzhewu@sfmail.sf-express.com>
2020-12-06 16:01:22 +0800

    add

--- packages/plugin-b/lib/plugin-b.js
+++ packages/plugin-b/lib/plugin-b.js
@@ -1,13 +1,10 @@
 'use strict';
 const react = require('react')
 const webpack = require('webpack')
-const PluginA = require('@monorepo/plugin-a')
 
 // console.log('res', react)
 module.exports = pluginB;
 
 function pluginB() {
-    // TODO
-    PluginA()
     console.log('我是插件B 111115555')
 }
